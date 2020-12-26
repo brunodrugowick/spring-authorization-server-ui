249d40ab5192bb496dde3df47fee9fecf01733db
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-12-26 15:16:22 -0300

    Adds a rudimentary (of course) UI via Stimulus.

---
+++ src/main/java/dev/drugowick/springauthorizationserverui/web/pages/StimulusController.java
@@ -0,0 +1,43 @@
+package dev.drugowick.springauthorizationserverui.web.pages;
+
+import dev.drugowick.springauthorizationserverui.domain.repository.ClientRepository;
+import dev.drugowick.springauthorizationserverui.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * A test controller for Stimulus on the front-end.
+ *
+ * I'm experimenting...
+ */
+@Controller
+@RequiredArgsConstructor
+public class StimulusController extends BasePageController {
+
+    private final UserRepository userRepository;
+    private final ClientRepository clientRepository;
+
+    @RequestMapping(path = "/stimulus-index.html")
+    public String getStimulusHome() {
+        return "stimulus/stimulus-index";
+    }
+
+    @RequestMapping(path = "/stimulus/header")
+    public String getAppHeader() {
+        return "fragments/header";
+    }
+
+    @RequestMapping(path = "/stimulus/users")
+    public String getUsersList(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "stimulus/users-list";
+    }
+
+    @RequestMapping(path = "/stimulus/clients")
+    public String getClientsList(Model model) {
+        model.addAttribute("clients", clientRepository.findAll());
+        return "stimulus/clients-list";
+    }
+}
