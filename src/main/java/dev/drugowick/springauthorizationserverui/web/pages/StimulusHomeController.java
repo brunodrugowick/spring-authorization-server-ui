e4891ea181e90750c30722bfba01a97b5a04b2fb
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-12-28 19:44:12 -0300

    Changes homepage to use Stimulus.
    Still not a good usage of it... just made it work.

--- src/main/java/dev/drugowick/springauthorizationserverui/web/pages/StimulusHomeController.java
+++ src/main/java/dev/drugowick/springauthorizationserverui/web/pages/StimulusHomeController.java
@@ -7,20 +7,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * A test controller for Stimulus on the front-end.
+ *
+ * I'm experimenting...
+ */
 @Controller
 @RequiredArgsConstructor
-public class HomePageController extends BasePageController {
+public class StimulusHomeController extends BasePageController {
 
     private final UserRepository userRepository;
     private final ClientRepository clientRepository;
 
-    @RequestMapping(path = "/")
-    public String index(Model model) {
-
-        model.addAttribute("users", userRepository.findAll());
-        model.addAttribute("clients", clientRepository.findAll());
-
-        return "index";
+    @RequestMapping(path = "/stimulus/header")
+    public String getAppHeader() {
+        return "fragments/header";
     }
 
+    @RequestMapping(path = "/stimulus/users")
+    public String getUsersList(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "stimulus/users-list";
+    }
+
+    @RequestMapping(path = "/stimulus/clients")
+    public String getClientsList(Model model) {
+        model.addAttribute("clients", clientRepository.findAll());
+        return "stimulus/clients-list";
+    }
 }
